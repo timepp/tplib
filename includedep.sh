bce09f598634227713d79978e2c10758e44fb3d0
timepp <tongjunhui@live.cn>
2010-12-26 22:50:24 +0800

    tp lib to be continued

---
+++ includedep.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cmd=${1:-dot}
+opt=""
+if [[ ${2} == "showsys" ]]; then opt=--include=forceshow; fi
+
+dir=""
+for drive in e d
+do
+	dir=/$drive/greensoft/psoft
+	if [ -d $dir ]; then break; fi
+done
+
+rm -f ./tplib.dot
+$dir/cmdline/cinclude2dot.pl --src=include $opt > ./tplib.dot
+
+if [ -d $dir/graphviz ]
+then
+	rm -f /tmp/tplib.ps
+	echo using $dir/graphviz/bin/$cmd.exe
+	$dir/graphviz/bin/$cmd.exe ./tplib.dot -Tps -o ./tplib.ps
+	if [ -f ./tplib.ps ]
+	then 
+		rm -f ./tplib.dot
+		cygstart ./tplib.ps
+	fi
+fi
+
---
+++ test/main.cpp
@@ -0,0 +1,14 @@
+#define _WIN32_WINNT 0x0501
+
+#include <windows.h>
+#include <tplib.h>
+#include <cmdlineparser.h>
+#include <exception.h>
+#include <wincon.h>
+#include <opblock.h>
+
+int wmain(int argc, wchar_t *argv[])
+{
+	ON_LEAVE(printf("afdsafs\n"));
+	return 0;
+}
