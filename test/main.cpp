ce316238411435ad2d079829bdabd2cad5683064
timepp <tongjunhui@live.cn>
2012-07-29 22:42:31 +0800

    单元测试

--- test/main.cpp
+++ test/main.cpp
@@ -6,17 +6,33 @@
 #include <exception.h>
 #include <wincon.h>
 #include <opblock.h>
+#include <pinyin.h>
+#include <unittest.h>
+
+#define TPUT_MODNAME Main
+
+TPUT_DEFINE_BLOCK(L"pinyin", L"")
+{
+	tp::pinyintool pyt;
+	TPUT_EXPECT(pyt.pinyin_of_string(L"龟骨粉") == L"gui gu fen");
+	TPUT_EXPECT(pyt.pinyin_of_string(L"今年CPI是多少?") == L"jin nian CPI shi duo shao ?");
+	TPUT_EXPECT(pyt.pinyin_of_string(L"peach是桃的意思") == L"peach shi tao di yi si");
+}
+
+TPUT_DEFINE_BLOCK(L"commandline parser", L"")
+{
+	tp::cmdline_parser parser;
+
+	parser.parse(L"");
+	TPUT_EXPECT_WITH_MSG(parser.get_targets().size() == 0, L"空命令行参数");
+
+	parser.parse(L"ccc.exe /x \"b b\" --aaaaa -- \"ffff gggg hhhh\" 'gggg'");
+	TPUT_EXPECT(parser.get_targets().size() == 6);
+}
 
 int wmain(int argc, wchar_t *argv[])
 {
-	tp::sc::log_default_console_config();
-	tp::log(L"main enter");
-	ON_LEAVE(tp::log(L"main leave"));
-
-	tp::cmdline_parser parser;
-	parser.parse(argc, argv);
-
-	tp::log(tp::cz(L"param count: %d, target count: %d", argc, parser.get_target_count()));
-
+	setlocale(LC_ALL, "chs");
+	tp::unittest::instance().run_test();
 	return 0;
 }
