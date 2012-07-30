a8d70e9eef8c435e9589327ea7fa0ea9fce8d947
timepp <tongjunhui@live.cn>
2012-07-30 12:36:34 +0800

    类型转换的支持

---
+++ include/convert.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <string>
+#include <stdlib.h>
+
+namespace tp
+{
+
+struct cvt
+{
+	static int to_int(const wchar_t* s)
+	{
+		return _wtoi(s);
+	}
+	static int to_int(const std::wstring& s)
+	{
+		return to_int(s.c_str());
+	}
+
+	static bool to_bool(const wchar_t* s)
+	{
+		return to_int(s)? true : false;
+	}
+	static bool to_bool(const std::wstring& s)
+	{
+		return to_bool(s.c_str());
+	}
+};
+
+
+
+}
