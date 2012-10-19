2be3eb4f70b218ee13f68324e3c4c3df53ee8df0
tongjunhui <tongjunhui@live.cn>
2012-10-19 19:04:22 +0800

    增加两个文件：compisite和compositetreectrl

---
+++ include/composite.h
@@ -0,0 +1,315 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <algorithm>
+
+class component;
+
+class component_creator
+{
+public:
+	virtual component* create(const std::wstring& classname) = 0;
+	virtual const component* get_prototype(const std::wstring& classname) const = 0;
+	virtual bool configure(component* c) = 0;
+	virtual ~component_creator() {}
+};
+
+class serializer
+{
+public:
+	virtual std::wstring get_property(const std::wstring& name) const = 0;
+	virtual std::wstring get_name() const = 0;
+	virtual serializer* get_first_child() const = 0;
+	virtual serializer* get_next_sibling() const = 0;
+
+	virtual bool set_property(const std::wstring& name, const std::wstring& val) = 0;
+	virtual serializer* add_child(const std::wstring& name) = 0;
+	virtual ~serializer() {}
+};
+
+class component
+{
+public:
+	virtual component* clone(bool deep = true) const = 0;
+
+	virtual std::wstring classname() const = 0;
+	virtual std::wstring name() const = 0;
+	virtual std::wstring desc() const = 0;
+
+	virtual int compare(const component* c) const = 0;
+	virtual component* simplify() const = 0;
+
+	virtual size_t child_count() const = 0;
+	virtual component* get_child(size_t index) const = 0;
+	virtual bool add_child(component* child) = 0;
+	virtual bool del_child(size_t index) = 0;
+	virtual bool del_child(component* child) = 0;
+	virtual bool can_add_child(component* child) const = 0;
+	virtual bool can_del_child(size_t index) const = 0;
+
+	virtual bool load(component_creator* cc, serializer* s) = 0;
+	virtual bool save(component_creator* cc, serializer* s) const = 0;
+
+	virtual ~component() {}
+};
+
+struct component_less
+{
+	bool operator()(const component* a, const component* b) const
+	{
+		return a->compare(b) < 0;
+	}
+};
+struct component_equal
+{
+	bool operator()(const component* a, const component* b) const
+	{
+		return a->compare(b) == 0;
+	}
+};
+
+class composite : public component
+{
+public:
+	virtual size_t child_count() const
+	{
+		return m_childs.size();
+	}
+	virtual component* get_child(size_t index) const
+	{
+		if (index >= m_childs.size()) return 0;
+		return m_childs[index];
+	}
+	virtual bool add_child(component* child)
+	{
+		if (!can_add_child(child))
+		{
+			return false;
+		}
+		for (size_t i = 0; i < child_count(); i++)
+		{
+			if (m_childs[i] == child)
+			{
+				return false;
+			}
+		}
+		m_childs.push_back(child);
+		return true;
+	}
+	virtual bool del_child(size_t index)
+	{
+		if (can_del_child(index) && index < m_childs.size())
+		{
+			component* c = m_childs[index];
+			m_childs.erase(m_childs.begin() + static_cast<int>(index));
+			delete c;
+			return true;
+		}
+		return false;
+	}
+	virtual bool del_child(component* child)
+	{
+		for (size_t i = 0; i < child_count(); i++)
+		{
+			if (m_childs[i] == child)
+			{
+				return del_child(i);
+			}
+		}
+
+		return false;
+	}
+
+	virtual bool can_add_child(component* /*child*/) const
+	{
+		return true;
+	}
+	virtual bool can_del_child(size_t /*index*/) const
+	{
+		return true;
+	}
+	virtual bool load(component_creator* cc, serializer* s)
+	{
+		serializer* child_serializer = s->get_first_child();
+		while (child_serializer)
+		{
+			component* child = cc->create(child_serializer->get_name());
+			if (child)
+			{
+				child->load(cc, child_serializer);
+				if (can_add_child(child))
+				{
+					add_child(child);
+				}
+			}
+			serializer* next_childs = child_serializer->get_next_sibling();
+			delete child_serializer;
+			child_serializer = next_childs;
+		}
+		return true;
+	}
+	virtual bool save(component_creator* cc, serializer* s) const
+	{
+		serializer* child_serializer = s->add_child(classname());
+		for (size_t i = 0; i < child_count(); i++)
+		{
+			component* c = get_child(i);
+			c->save(cc, child_serializer);
+		}
+		delete child_serializer;
+		return true;
+	}
+
+	virtual int compare(const component* c) const
+	{
+		if (c == static_cast<const component*>(this)) return 0;
+
+		const composite* comp = dynamic_cast<const composite*>(c);
+		if (!comp)
+		{
+			// composite > leaf
+			return 1;
+		}
+
+		int r = name().compare(comp->name());
+		if (r != 0) return r;
+
+		return compare_composite(comp);
+	}
+
+	virtual int compare_composite(const composite* c) const
+	{
+		size_t n1 = child_count();
+		size_t n2 = c->child_count();
+		if (n1 < n2) return -1;
+		if (n1 > n2) return 1;
+
+		for (size_t i = 0; i < child_count(); i++)
+		{
+			component* c1 = get_child(i);
+			component* c2 = c->get_child(i);
+			int rr = c1->compare(c2);
+			if (rr < 0) return -1;
+			if (rr > 0) return 1;
+		}
+
+		return 0;
+	}
+
+	virtual void sort()
+	{
+		std::sort(m_childs.begin(), m_childs.end(), component_less());
+	}
+	virtual void uniq()
+	{
+		for (size_t i = 0;;)
+		{
+			if (i + 1 >= child_count()) break;
+
+			if (get_child(i)->compare(get_child(i+1)) == 0)
+			{
+				del_child(i+1);
+			}
+			else
+			{
+				i++;
+			}
+		}
+	}
+
+	virtual component* simplify() const
+	{
+		composite* c = dynamic_cast<composite*>(clone(false));
+
+		// child first
+		for (childs_t::const_iterator it = m_childs.begin(); it != m_childs.end(); ++it)
+		{
+			component* child = (*it)->simplify();
+			if (child) c->add_child(child);
+		}
+
+		//
+		if (c->child_count() == 0)
+		{
+			delete c;
+			return NULL;
+		}
+
+		// pass direct
+		if (c->child_count() == 1 && c->equal_to_child())
+		{
+			component* ret = c->get_child(0)->clone();
+			delete c;
+			return ret;
+		}
+
+		// 
+		if (compare(c) != 0)
+		{
+			component* ret = c->simplify();
+			delete c;
+			return ret;
+		}
+
+		return c;
+	}
+
+	~composite()
+	{
+		for (size_t i = 0; i < child_count(); i++)
+		{
+			delete get_child(i);
+		}
+	}
+
+protected:
+	typedef std::vector<component*> childs_t;
+	childs_t m_childs;
+
+	void clone_childs(childs_t* childs) const
+	{
+		if (!childs) return;
+
+		childs->clear();
+		for (childs_t::const_iterator it = m_childs.begin(); it != m_childs.end(); ++it)
+		{
+			childs->push_back((*it)->clone());
+		}
+	}
+
+	virtual bool equal_to_child() const
+	{
+		return (child_count() == 1);
+	}
+};
+
+class leaf : public component
+{
+public:
+	virtual size_t child_count() const                      { return 0;     }
+	virtual component* get_child(size_t /*index*/) const    { return NULL;  }
+	virtual bool add_child(component* /*child*/)            { return false; }
+	virtual bool del_child(size_t /*index*/)                { return false; }
+	virtual bool del_child(component* /*child*/)            { return false; }
+	virtual bool can_add_child(component* /*child*/) const  { return false; }
+	virtual bool can_del_child(size_t /*index*/) const      { return false; }
+
+	virtual int compare(const component* c) const
+	{
+		const leaf* l = dynamic_cast<const leaf*>(c);
+		if (!l)
+		{
+			// leaf < composite
+			return -1;
+		}
+
+		int r = name().compare(l->name());
+		if (r != 0) return r;
+
+		return compare(l);
+	}
+
+private:
+	virtual int compare(const leaf* l) const = 0;
+};
---
+++ include/compositetreectrl.h
@@ -0,0 +1,559 @@
+#pragma once
+
+#include "composite.h"
+#include <wtl/atlctrls.h>
+
+#define MENUID_COPY               1001
+#define MENUID_PASTE              1002
+#define MENUID_PASTE_AS_CHILD     1003
+#define MENUID_CUT                1004
+#define MENUID_DELETE             1005
+#define MENUID_ADD_COMP_BEGIN     1100
+#define MENUID_ADD_COMP_END       1199
+#define MENUID_CVT_COMP_BEGIN     1200
+#define MENUID_CVT_COMP_END       1299
+#define MENUID_ADD_COMP_AS_PARENT_BEGIN 1300
+#define MENUID_ADD_COMP_AS_PARENT_END   1399
+
+#define KEY_IS_DOWN(vk) ((GetKeyState(vk) & 0x8000) != 0)
+
+class CRootComposite : public composite
+{
+public:
+	virtual std::wstring name() const { return L""; }
+	virtual std::wstring desc() const { return L""; }
+	virtual component* clone(bool deep = true) const
+	{
+		CRootComposite* c = new CRootComposite;
+		if (deep) clone_childs(&c->m_childs);
+		return c;
+	}
+	virtual bool can_add_child(component* /*child*/) const
+	{
+		return child_count() == 0;
+	}
+	virtual bool add_child(component* child)
+	{
+		for (size_t i = 0; i < child_count(); i++)
+		{
+			if (m_childs[i] == child)
+			{
+				return false;
+			}
+		}
+		m_childs.push_back(child);
+		return true;
+	}
+	virtual bool load(component_creator* /*cc*/, serializer* /*s*/) { return false; }
+	virtual bool save(component_creator* /*cc*/, serializer* /*s*/) const { return false; }
+	virtual std::wstring classname() const { return L"CRootComposite"; }
+};
+
+class CCompositeTreeCtrl : public CWindowImpl<CCompositeTreeCtrl, CTreeViewCtrlEx>
+{
+public:
+	BEGIN_MSG_MAP(CCompositeTreeCtrl)
+		MSG_WM_RBUTTONDOWN(OnRbuttonDown)
+		MSG_WM_LBUTTONDBLCLK(OnLbuttonDbclk)
+		MSG_WM_LBUTTONDOWN(OnLButtonDown)
+		MSG_WM_MOUSEMOVE(OnMouseMove)
+		MSG_WM_LBUTTONUP(OnLButtonUp)
+		MSG_WM_COMMAND(OnCommand)
+		MSG_WM_KEYDOWN(OnKeyDown)
+		MSG_WM_CONTEXTMENU(OnContextMenu)
+	END_MSG_MAP()
+
+	CCompositeTreeCtrl(): m_clipboard(NULL), m_fc(NULL), m_lb_down(false), m_dragging(false)
+	{
+	}
+	~CCompositeTreeCtrl()
+	{
+	}
+
+	void Create(HWND hWndParent, HIMAGELIST imglist)
+	{
+		CWindowImpl<CCompositeTreeCtrl, CTreeViewCtrlEx>::Create(
+			hWndParent, NULL, NULL,
+			CControlWinTraits::GetWndStyle(0) | WS_BORDER | WS_TABSTOP | TVS_DISABLEDRAGDROP | TVS_HASLINES | TVS_HASBUTTONS | TVS_LINESATROOT | TVS_INFOTIP
+			);
+		m_imgList = SetImageList(imglist, TVSIL_NORMAL);
+	}
+
+	void SetComponentCreator(component_creator* fc)
+	{
+		m_fc = fc;
+	}
+
+	void AddComponentName(const std::wstring& cname)
+	{
+		m_cnames.push_back(cname);
+	}
+
+	void SetComponent(const component* c)
+	{
+		CTreeItem item = GetRootItem();
+		if (!IsNormalItem(item))
+		{
+			if (c)
+				InsertComponent(TVI_ROOT, TVI_LAST, c->clone());
+		}
+		else
+		{
+			if (c)
+				InsertComponent(TVI_ROOT, item, c->clone());
+			DeleteComponent(item);
+		}
+	}
+
+	component* GetComponent() const
+	{
+		if (m_root.child_count() == 0)
+		{
+			return NULL;
+		}
+		return m_root.get_child(0);
+	}
+
+private:
+	CRootComposite m_root;
+	component* m_clipboard;
+	component_creator* m_fc;
+	std::vector<std::wstring> m_cnames;
+	CImageList m_dragImgList;
+	CImageList m_imgList;
+
+	// status
+	bool m_lb_down;
+	bool m_dragging;
+	CPoint m_dragpt;
+	CTreeItem m_dragging_source;
+	CTreeItem m_dragging_target;
+
+
+	void PopupContextMenu(CPoint pt)
+	{
+		CMenu muAdd;
+		CMenu muAddAsParent;
+		CMenu muCvt;
+		muAdd.CreatePopupMenu();
+		muAddAsParent.CreatePopupMenu();
+		muCvt.CreatePopupMenu();
+		for (size_t i = 0; i < m_cnames.size(); i++)
+		{
+			if (m_cnames[i].length() == 0)
+			{
+				muAdd.AppendMenu(MF_SEPARATOR);
+				muCvt.AppendMenu(MF_SEPARATOR);
+			}
+			else
+			{
+				const component* proto = m_fc->get_prototype(m_cnames[i]);
+				muAdd.AppendMenu(MF_STRING, MENUID_ADD_COMP_BEGIN + i, proto->name().c_str());
+				muCvt.AppendMenu(MF_STRING, MENUID_CVT_COMP_BEGIN + i, proto->name().c_str());
+				if (proto->can_add_child(NULL))
+				{
+					muAddAsParent.AppendMenu(MF_STRING, MENUID_ADD_COMP_AS_PARENT_BEGIN + i, proto->name().c_str());
+				}
+			}
+		}
+		
+		CMenu mu;
+		mu.CreatePopupMenu();
+		mu.AppendMenu(MF_POPUP, muAdd.m_hMenu, L"添加(&A)");
+		mu.AppendMenu(MF_POPUP, muAddAsParent.m_hMenu, L"添加父节点(&Q)");
+		mu.AppendMenu(MF_POPUP, muCvt.m_hMenu, L"切换为(&S)");
+		mu.AppendMenu(MF_SEPARATOR);
+		mu.AppendMenu(MF_STRING, MENUID_COPY, L"复制(&C)\tCtrl+C");
+		mu.AppendMenu(MF_STRING, MENUID_CUT, L"剪切(&X)\tCtrl+X");
+		mu.AppendMenu(MF_STRING, MENUID_PASTE, L"粘贴(&V)\tCtrl+V");
+		mu.AppendMenu(MF_STRING, MENUID_PASTE_AS_CHILD, L"粘贴为子结点(&B)");
+		mu.AppendMenu(MF_STRING, MENUID_DELETE, L"删除(&D)\tDel");
+
+		CTreeItem item = GetSelectedItem();
+		component* c = GetItemData(item);
+		if (!IsNormalItem(item))
+		{
+			mu.EnableMenuItem(MENUID_COPY, MF_DISABLED);
+			mu.EnableMenuItem(MENUID_CUT, MF_DISABLED);
+			mu.EnableMenuItem(MENUID_DELETE, MF_DISABLED);
+			mu.EnableMenuItem((UINT)muCvt.m_hMenu, MF_DISABLED);
+			mu.EnableMenuItem((UINT)muAddAsParent.m_hMenu, MF_DISABLED);
+			mu.EnableMenuItem(MENUID_PASTE, MF_DISABLED);
+		}
+		else
+		{
+			CTreeItem parentItem = item.GetParent();
+			component* pc = GetItemData(parentItem);
+			if (!pc->can_add_child(NULL))
+			{
+				mu.EnableMenuItem(MENUID_PASTE, MF_DISABLED);
+			}
+		}
+		
+		if (!c->can_add_child(NULL))
+		{
+			mu.EnableMenuItem((UINT)muAdd.m_hMenu, MF_DISABLED);
+			mu.EnableMenuItem(MENUID_PASTE_AS_CHILD, MF_DISABLED);
+		}
+
+		if (!m_clipboard)
+		{
+			mu.EnableMenuItem(MENUID_PASTE, MF_DISABLED);
+			mu.EnableMenuItem(MENUID_PASTE_AS_CHILD, MF_DISABLED);
+		}
+
+		ClientToScreen(&pt);
+		mu.TrackPopupMenu(0, pt.x, pt.y, m_hWnd);
+	}
+
+	// 复合结点前显示名字前缀
+	std::wstring GetComponentDisplayText(const component* c)
+	{
+		if (dynamic_cast<const composite*>(c))
+		{
+			return c->name() + L"：" + c->desc();
+		}
+		else if (c)
+		{
+			return c->desc();
+		}
+		return L"";
+	}
+
+	void UpdateDesc(CTreeItem item)
+	{
+		while (IsNormalItem(item))
+		{
+			component* c = GetItemData(item);
+			item.SetText(GetComponentDisplayText(c).c_str());
+			item = item.GetParent();
+		}
+	}
+
+	void OnRbuttonDown(UINT /*code*/, CPoint pt)
+	{
+		SetFocus();
+		CTreeItem item = HitTest(pt, NULL);
+		Select(item, TVGN_CARET);
+	}
+
+	void OnContextMenu(HWND /*hWnd*/, CPoint pt)
+	{
+		if (pt.x == -1 || pt.y == -1)
+		{
+			CTreeItem item = GetSelectedItem();
+			if (!item.IsNull())
+			{
+				CRect rc;
+				item.GetRect(&rc, TRUE);
+				PopupContextMenu(CPoint(rc.left, rc.bottom));
+			}
+		}
+		else
+		{
+			ScreenToClient(&pt);
+			PopupContextMenu(pt);
+		}
+	}
+
+	void OnLbuttonDbclk(UINT /*code*/, CPoint pt)
+	{
+		CTreeItem item = HitTest(pt, NULL);
+		component* c = GetItemData(item);
+
+		m_fc->configure(c);
+		UpdateDesc(item);
+
+		SetMsgHandled(FALSE);
+	}
+
+	component* GetItemData(CTreeItem item)
+	{
+		if (!IsNormalItem(item))
+		{
+			return &m_root;
+		}
+		return reinterpret_cast<component*>(item.GetData());
+	}
+
+	bool IsNormalItem(CTreeItem item)
+	{
+		return !item.IsNull() && item != TVI_ROOT;
+	}
+
+	void DeleteComponent(CTreeItem item)
+	{
+		CTreeItem parentItem = item.GetParent();
+		GetItemData(parentItem)->del_child(GetItemData(item));
+		DeleteItem(item);
+		UpdateDesc(parentItem);
+	}
+
+	void CopyToClipboard(component* c)
+	{
+		if (c)
+		{
+			if (m_clipboard) delete m_clipboard;
+			m_clipboard = c->clone();
+		}
+	}
+
+	int GetComponentIconIndex(const component* c)
+	{
+		std::wstring name = c->classname();
+		for (size_t i = 0; i < m_cnames.size(); i++)
+		{
+			if (m_cnames[i] == name) return static_cast<int>(i);
+		}
+		return -1;
+	}
+
+	void InsertComponent(CTreeItem parentItem, CTreeItem item, component* c)
+	{
+		int index = GetComponentIconIndex(c);
+		CTreeItem newItem = InsertItem(GetComponentDisplayText(c).c_str(), index, index, parentItem, item);
+		newItem.SetData((DWORD_PTR)c);
+
+		GetItemData(parentItem)->add_child(c);
+
+		for (size_t i = 0; i < c->child_count(); i++)
+		{
+			InsertComponent(newItem, TVI_LAST, c->get_child(i));
+		}
+
+		if (IsNormalItem(parentItem))
+		{
+			parentItem.Expand();
+			UpdateDesc(parentItem);
+		}
+	}
+
+	void OnCommand(UINT /*code*/, int id, HWND /*hWnd*/)
+	{
+		if (id >= MENUID_ADD_COMP_BEGIN && id <= MENUID_ADD_COMP_END)
+		{
+			component* c = m_fc->create(m_cnames[static_cast<size_t>(id - MENUID_ADD_COMP_BEGIN)]);
+			m_fc->configure(c);
+			CTreeItem item = GetSelectedItem();
+			InsertComponent(item, TVI_LAST, c);
+		}
+		else if (id >= MENUID_CVT_COMP_BEGIN && id <= MENUID_CVT_COMP_END)
+		{
+			component* c = m_fc->create(m_cnames[static_cast<size_t>(id - MENUID_CVT_COMP_BEGIN)]);
+			m_fc->configure(c);
+			CTreeItem item = GetSelectedItem();
+			CTreeItem parentItem = item.GetParent();
+			component* oldc = GetItemData(item);
+			for (size_t i = 0; i < oldc->child_count(); i++)
+			{
+				c->add_child(oldc->get_child(i)->clone());
+			}
+			InsertComponent(parentItem, item, c);
+			DeleteComponent(item);
+		}
+		else if (id >= MENUID_ADD_COMP_AS_PARENT_BEGIN && id <= MENUID_ADD_COMP_AS_PARENT_END)
+		{
+			component* c = m_fc->create(m_cnames[static_cast<size_t>(id - MENUID_ADD_COMP_AS_PARENT_BEGIN)]);
+			m_fc->configure(c);
+			CTreeItem item = GetSelectedItem();
+			CTreeItem parentItem = item.GetParent();
+			component* me = GetItemData(item);
+			c->add_child(me->clone());
+			InsertComponent(parentItem, item, c);
+			DeleteComponent(item);
+		}
+		else if (id == MENUID_DELETE)
+		{
+			CTreeItem item = GetSelectedItem();
+			DeleteComponent(item);
+		}
+		else if (id == MENUID_CUT)
+		{
+			CTreeItem item = GetSelectedItem();
+			if (!item.IsNull())
+			{
+				CopyToClipboard(GetItemData(item));
+				DeleteComponent(item);
+			}
+		}
+		else if (id == MENUID_COPY)
+		{
+			CTreeItem item = GetSelectedItem();
+			if (!item.IsNull())
+			{
+				CopyToClipboard(GetItemData(item));
+			}
+		}
+		else if (id == MENUID_PASTE)
+		{
+			CTreeItem item = GetSelectedItem();
+			CTreeItem parentItem = item.GetParent();
+			if (m_clipboard && GetItemData(parentItem)->can_add_child(NULL))
+			{
+				InsertComponent(parentItem, item, m_clipboard->clone());
+			}
+		}
+		else if (id == MENUID_PASTE_AS_CHILD)
+		{
+			CTreeItem item = GetSelectedItem();
+			if (m_clipboard && GetItemData(item)->can_add_child(NULL))
+			{
+				InsertComponent(item, TVI_LAST, m_clipboard->clone());
+			}
+		}
+	}
+
+	void SelectItemIfValid(CTreeItem item)
+	{
+		if (!item.IsNull()) item.Select(TVGN_CARET);
+	}
+
+	void OnKeyDown(UINT ch, UINT /*cnt*/, UINT /*flags*/)
+	{
+		bool bControlKeyIsDown = KEY_IS_DOWN(VK_CONTROL);
+		bool bShiftKeyIsDown = KEY_IS_DOWN(VK_SHIFT);
+		if (ch == VK_DELETE)
+		{
+			SendMessage(WM_COMMAND, MENUID_DELETE, 0);
+		}
+		else if (ch == L'C' && bControlKeyIsDown)
+		{
+			SendMessage(WM_COMMAND, MENUID_COPY, 0);
+		}
+		else if (ch == L'X' && bControlKeyIsDown)
+		{
+			SendMessage(WM_COMMAND, MENUID_CUT, 0);
+		}
+		else if (ch == L'V' && bControlKeyIsDown)
+		{
+			SendMessage(WM_COMMAND, (WPARAM)(bShiftKeyIsDown? MENUID_PASTE_AS_CHILD : MENUID_PASTE), 0);
+		}
+		else if (ch == VK_LEFT)
+		{
+			CTreeItem item = GetSelectedItem();
+			if (item.HasChildren() && item.GetState(TVIS_EXPANDED))
+			{
+				item.Expand(TVE_COLLAPSE);
+			}
+			else
+			{
+				SelectItemIfValid(item.GetParent());
+			}
+		}
+		else if (ch == VK_RIGHT)
+		{
+			CTreeItem item = GetSelectedItem();
+			if (item.HasChildren() && !item.GetState(TVIS_EXPANDED))
+			{
+				item.Expand(TVE_EXPAND);
+			}
+			else
+			{
+				SelectItemIfValid(item.GetChild());
+			}
+		}
+		else if (ch == VK_DOWN)
+		{
+			SelectItemIfValid(GetSelectedItem().GetNextVisible());
+		}
+		else if (ch == VK_UP)
+		{
+			SelectItemIfValid(GetSelectedItem().GetPrevVisible());
+		}
+	}
+
+	int Distance(CPoint pt1, CPoint pt2)
+	{
+		return (pt1.x - pt2.x) * (pt1.x - pt2.x) + (pt1.y - pt2.y) * (pt1.y - pt2.y);
+	}
+
+	bool IsDescendant(CTreeItem t, CTreeItem p)
+	{
+		if (t.IsNull() || p.IsNull()) return false;
+		do 
+		{
+			t = t.GetParent();
+			if (t == p) return true;
+		} while (!t.IsNull());
+		return false;
+	}
+
+	void DragComponent(CTreeItem src, CTreeItem dest, bool copy)
+	{
+		if (src == dest || IsDescendant(dest, src))
+		{
+			return;
+		}
+
+		if (GetItemData(dest)->can_add_child(NULL))
+		{
+			InsertComponent(dest, TVI_LAST, GetItemData(src)->clone());
+			if (!copy)
+			{
+				DeleteComponent(src);
+			}
+		}
+	}
+
+	void OnLButtonDown(UINT /*code*/, CPoint pt)
+	{
+		m_lb_down = true;
+		UINT flag;
+		CTreeItem item = HitTest(pt, &flag);
+		if (IsNormalItem(item))
+		{
+			Select(item, TVGN_CARET);
+			SetMsgHandled(FALSE);
+			if (flag & TVHT_ONITEM)
+			{
+				m_dragging_source = item;
+				m_dragging_target = NULL;
+				m_dragpt = pt;
+			}
+		}
+	}
+
+	void OnMouseMove(UINT /*code*/, CPoint pt)
+	{
+		if (m_dragging)
+		{
+			UINT flag;
+			CTreeItem item = HitTest(pt, &flag);
+			if (item != m_dragging_target)
+			{
+				m_dragImgList.DragShowNolock(FALSE);
+				Select(item, TVGN_CARET);
+				m_dragImgList.DragShowNolock(TRUE);
+				m_dragging_target = item;
+			}
+			m_dragImgList.DragMove(pt);
+			return;
+		}
+		if (m_lb_down && !m_dragging_source.IsNull() && Distance(pt, m_dragpt) >= 100)
+		{
+			m_dragging = true;
+			m_dragImgList = CreateDragImage(m_dragging_source);
+			m_dragImgList.BeginDrag(0, CPoint(0,0));
+			m_dragImgList.DragEnter(m_hWnd, pt);
+			SetCapture();
+		}
+	}
+
+	void OnLButtonUp(UINT /*code*/, CPoint /*pt*/)
+	{
+		m_lb_down = false;
+		ReleaseCapture();
+		if (m_dragging)
+		{
+			m_dragImgList.DragLeave(m_hWnd);
+			m_dragImgList.EndDrag();
+			if (!m_dragging_source.IsNull() && !m_dragging_target.IsNull())
+			{
+				DragComponent(m_dragging_source, m_dragging_target, KEY_IS_DOWN(VK_CONTROL));
+			}
+		}
+		m_dragging = false;
+		m_dragging_source = NULL;
+	}
+
+};
