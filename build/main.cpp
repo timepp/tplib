7f80991b9fcf5d2e2660c73c0d1ed3454f392ed0
tongjunhui <tongjunhui@live.cn>
2012-11-18 11:02:40 +0800

    删除旧的工程，移动目录

--- build/main.cpp
+++ build/main.cpp
@@ -1,4 +1,4 @@
-﻿#define _WIN32_WINNT 0x0501
+﻿#include "stdafx.h"
 
 #include <windows.h>
 #include <tplib.h>
---
+++ build/stdafx.h
@@ -0,0 +1,27 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#pragma warning(push, 1)
+#pragma warning(disable: 4350)
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <Windows.h>
+#include <vector>
+#include <list>
+#include <string>
+#include <map>
+
+#pragma warning(pop)
+
+
+// warning C4514: 'tp::opblock::opblock' : unreferenced inline function has been removed
+#pragma warning(disable: 4514)
+// warning C4710: 'std::string std::locale::name(void) const' : function not inlined
+#pragma warning(disable: 4710)
