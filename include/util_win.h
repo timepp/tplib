8c601c725d114922a3e2f96167e04660378416e6
tongjunhui <tongjunhui@live.cn>
2013-05-18 14:41:55 +0800

    解决翻译错误, 增加tp::oswin::list_dir

--- include/util_win.h
+++ include/util_win.h
@@ -1,14 +1,25 @@
-#ifndef TP_UTIL_WIN_H_INCLUDED
-#define TP_UTIL_WIN_H_INCLUDED
+#pragma once
 
 #include "exception.h"
 #include "auto_release.h"
 #include <vector>
+#include <string>
+#include <windows.h>
 
 namespace tp
 {
-	struct oswin
+	namespace oswin
 	{
+		struct file
+		{
+			std::wstring abs_path;
+			std::wstring rela_path;
+			FILETIME create_time;
+			FILETIME modify_time;
+			FILETIME access_time;
+			UINT64 length;
+			DWORD attribute;
+		};
 		class iniconf
 		{
 			std::wstring m_path;
@@ -111,11 +122,11 @@ namespace tp
 		{
 			HKEY hkey;
 			LONG ret = RegOpenKeyW(root, path.c_str(), &hkey);
-			throw_if_lasterror(ret);
+			throw_winerr_when(ret != ERROR_SUCCESS);
 			ON_LEAVE_1(RegCloseKey(hkey), HKEY, hkey);
 
 			ret = RegDeleteKeyW(hkey, name.c_str());
-			throw_if_lasterror(ret);
+			throw_winerr_when(ret != ERROR_SUCCESS);
 		}
 
 		void reg_deletekey(HKEY root, const std::wstring& pathname)
@@ -130,22 +141,22 @@ namespace tp
 		{
 			HKEY hkey;
 			LONG ret = RegCreateKeyExW(root, path.c_str(), 0, NULL, 0, KEY_ALL_ACCESS, 0, &hkey, NULL);
-			throw_if_lasterror(ret);
+			throw_winerr_when(ret != ERROR_SUCCESS);
 			ON_LEAVE_1(RegCloseKey(hkey), HKEY, hkey);
 
 			ret = RegSetValueExW(hkey, name.c_str(), 0, REG_SZ, (CONST BYTE*)value.c_str(), (value.length() + 1) * 2);
-			throw_if_lasterror(ret);
+			throw_winerr_when(ret != ERROR_SUCCESS);
 		}
 
 		void reg_write_dword(HKEY root, LPCWSTR path, LPCWSTR name, DWORD value)
 		{
 			HKEY hkey;
 			LONG ret = RegCreateKeyExW(root, path, 0, NULL, 0, KEY_ALL_ACCESS, 0, &hkey, NULL);
-			throw_if_lasterror(ret);
+			throw_winerr_when(ret != ERROR_SUCCESS);
 			ON_LEAVE_1(RegCloseKey(hkey), HKEY, hkey);
 
 			ret = RegSetValueExW(hkey, name, 0, REG_DWORD, (CONST BYTE*)&value, 4);
-			throw_if_lasterror(ret);
+			throw_winerr_when(ret != ERROR_SUCCESS);
 		}
 
 		void reg_read_value(HKEY root, const std::wstring& path, const std::wstring& name, DWORD& type, LPBYTE buffer, LPDWORD buflen)
@@ -153,11 +164,11 @@ namespace tp
 			HKEY hkey;
 			LSTATUS ret;
 			ret = RegOpenKeyW(root, path.c_str(), &hkey);
-			throw_if_lasterror(ret);
+			throw_winerr_when(ret != ERROR_SUCCESS);
 			ON_LEAVE_1(RegCloseKey(hkey), HKEY, hkey);
 
 			ret = RegQueryValueExW(hkey, name.c_str(), NULL, &type, buffer, buflen);
-			throw_if_lasterror(ret);
+			throw_winerr_when(ret != ERROR_SUCCESS);
 		}
 
 		std::wstring reg_read_string(HKEY root, const std::wstring& path, const std::wstring& name, const std::wstring& defval)
@@ -187,7 +198,7 @@ namespace tp
 			HKEY hkey;
 			LSTATUS ret;
 			ret = RegOpenKeyW(HKEY_LOCAL_MACHINE, path.c_str(), &hkey);
-			throw_if_lasterror(ret);
+			throw_winerr_when(ret != ERROR_SUCCESS);
 			ON_LEAVE_1(RegCloseKey(hkey), HKEY, hkey);
 
 			WCHAR subKey[1024];
@@ -209,7 +220,7 @@ namespace tp
 			DWORD dwMaxValueLen = 16383;
 			LSTATUS ret;
 			ret = RegQueryInfoKeyW(src, NULL, NULL, NULL, NULL, &dwMaxSubkeyLen, NULL, NULL, &dwMaxValueNameLen, &dwMaxValueLen, NULL, NULL);
-			throw_if_lasterror(ret);
+			throw_winerr_when(ret != ERROR_SUCCESS);
 
 			wchar_t* key = new wchar_t[dwMaxSubkeyLen + 1];
 			ON_LEAVE_1(delete [] key, wchar_t*, key);
@@ -225,9 +236,9 @@ namespace tp
 				DWORD dwType;
 				ret = RegEnumValueW(src, index, vname, &dwValueNameLen, NULL, &dwType, value, &dwValueLen);
 				if (ret == ERROR_NO_MORE_ITEMS) break;
-				throw_if_lasterror(ret);
+				throw_winerr_when(ret != ERROR_SUCCESS);
 				ret = RegSetValueExW(dest, vname, NULL, dwType, value, dwValueLen);
-				throw_if_lasterror(ret);
+				throw_winerr_when(ret != ERROR_SUCCESS);
 			}
 
 			for (DWORD index = 0;;index++)
@@ -235,20 +246,55 @@ namespace tp
 				DWORD dwKeyLen = dwMaxSubkeyLen + 1;
 				ret = RegEnumKeyExW(src, index, key, &dwKeyLen, NULL, NULL, NULL, NULL);
 				if (ret == ERROR_NO_MORE_ITEMS) break;
-				throw_if_lasterror(ret);
+				throw_winerr_when(ret != ERROR_SUCCESS);
 				HKEY newsrc;
 				HKEY newdst;
 				ret = RegOpenKeyExW(src, key, NULL, KEY_READ, &newsrc);
-				throw_if_lasterror(ret);
+				throw_winerr_when(ret != ERROR_SUCCESS);
 				ret = RegCreateKeyExW(dest, key, NULL, NULL, 0, KEY_READ|KEY_WRITE, NULL, &newdst, NULL);
-				throw_if_lasterror(ret);
+				throw_winerr_when(ret != ERROR_SUCCESS);
 
 				reg_copykey(newsrc, newdst);
 			}
 		}
 
+		static inline void list_dir(std::vector<file>& result, const wchar_t* path, const wchar_t* pattern = NULL, bool include_file = true, bool include_dir = false, bool recursive = false)
+		{
+			if (!pattern) pattern = L"*";
+			if (!path || !*path) path = L".";
+			std::wstring matcher = path;
+			matcher += L'\\';
+			matcher += pattern;
+
+			WIN32_FIND_DATA wfd;
+			HANDLE h = ::FindFirstFileW(matcher.c_str(), &wfd);
+			if (h == INVALID_HANDLE_VALUE) return;
+			ON_LEAVE_1(::FindClose(h), HANDLE, h);
+			std::wstring root = path;
+			file f;
+			do {
+				if (wfd.cFileName[0] == L'.' && !wfd.cFileName[1] ||
+					wfd.cFileName[0] == L'.' && wfd.cFileName[1] == L'.' && !wfd.cFileName[2])
+				{
+					continue;
+				}
+				bool isdir = wfd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY;
+				if (include_file && !isdir || include_dir && isdir)
+				{
+					f.rela_path = root + L"\\" + wfd.cFileName;
+					f.attribute = wfd.dwFileAttributes;
+					f.length = (UINT64)wfd.nFileSizeHigh << 32 + wfd.nFileSizeLow;
+					f.access_time = wfd.ftLastAccessTime;
+					f.modify_time = wfd.ftLastWriteTime;
+					f.create_time = wfd.ftCreationTime;
+					result.push_back(f);
+				}
+				if (isdir && recursive)
+				{
+					list_dir(result, f.rela_path.c_str(), pattern, include_file, include_dir, recursive);
+				}
+			} while (::FindNextFileW(h, &wfd));
+		}
 	};
 }
 
-
-#endif
